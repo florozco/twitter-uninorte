b9738b47b1c403c1568fa0dd1edc2f955afa3d2b
DiegoPalacios1512 <escalerad@uninorte.edu.co>
2021-09-10 12:17:15 -0500

    Retención de datos LogIn
    Se mantiene sesión iniciada

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Nav from "./components/common/Nav";
 import routes from "./lib/routes";
+import UserProvider from "./contexts/userContext";
 
 const App = () => {
+
   const { restricted, unrestricted } = routes;
+
   return (
     <main>
       <BrowserRouter>
         <h1>App</h1>
         <Nav />
-        <Switch>
-          {restricted.map(({ path, component }) => (
-            <Route exact path={path} component={component} />
-          ))}
-          {unrestricted.map(({ path, component }) => (
-            <Route exact path={path} component={component} />
-          ))}
-        </Switch>
+        <UserProvider>
+          <Switch>
+            {restricted.map(({ path, component }) => (
+              <Route exact path={path} component={component} />
+            ))}
+            {unrestricted.map(({ path, component }) => (
+              <Route exact path={path} component={component} />
+            ))}
+          </Switch>
+        </UserProvider>
       </BrowserRouter>
     </main>
   );
--- src/components/Public/Login/index.jsx
+++ src/components/Public/Login/index.jsx
@@ -3,51 +3,70 @@ import Label from "./../../common/Label";
 import Button from "./../../common/Button";
 import Form from "./../../common/Form";
 import Image from "../../../images/index";
-
+import { useUser } from "../../../contexts/userContext";
 import "../../../styles/displayStyles.scss";
+import { useState } from "react";
 
-const Login = () => (
-  <>
-    <Form className="center">
-      <img className="icon-img" src={Image.twitterIcon} /> <p></p>
-      <Label text="My Twitter" className="my-twitter-label" />
-      <p></p>
-      <Label text="Login to your account" className="login-label" /> <p></p>
-      <Label text="Email or Username" className="user-mail-pasword-label" />
-      <p></p>
-      <Input
-        type="text"
-        className="user-mail-input"
-        name="username"
-        id="username"
-        title="Username"
-      />
-      <p></p>
-      <Label text="Password" className="pasword-label" /> <p></p>
-      <Input
-        type="password"
-        className="password-input"
-        name="password"
-        id="password"
-        title="Password"
-      />
-      <p></p>
-      <Button
-        text="Forgot password?"
-        className="join-free-forgot-password-button"
-      />
-      <p></p>
-      <Button text="Login now" className="login-now-button" />
-      <p></p>
-      <a className="inline">
-        <Label text="Dont have an account?" className="no-account-label" />
+const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { username, setUser } = useUser()
+
+  if (username) {
+    console.log("Mandar al Home")
+  } else {
+    console.log("Mandar a LogIn")
+  }
+
+  const handleSubmit = (email, password) => {
+    setUser({ user: email, password: password })
+  }
+
+  return (
+    <>
+      <Form className="center">
+        <img className="icon-img" src={Image.twitterIcon} /> <p></p>
+        <Label text="My Twitter" className="my-twitter-label" />
+        <p></p>
+        <Label text="Login to your account" className="login-label" /> <p></p>
+        <Label text="Email" className="user-mail-pasword-label" />
+        <p></p>
+        <Input
+          type="text"
+          className="user-mail-input"
+          name="username"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <p></p>
+        <Label text="Password" className="pasword-label" /> <p></p>
+        <Input
+          type="password"
+          className="password-input"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p></p>
         <Button
-          text="Join free today"
+          text="Forgot password?"
           className="join-free-forgot-password-button"
         />
-      </a>
-    </Form>
-  </>
-);
+        <p></p>
+        <Button onClick={(e) => handleSubmit(email, password)} text="Login now" className="login-now-button" />
+        <p></p>
+        <a className="inline">
+          <Label text="Dont have an account?" className="no-account-label" />
+          <Button
+            text="Join free today"
+            className="join-free-forgot-password-button"
+          />
+        </a>
+      </Form>
+    </>
+  );
+};
 
 export default Login;
